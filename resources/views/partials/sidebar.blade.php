67f64397d327946350a72a5030f303d4a868fbcf
Falathin Nakamura <ibrahimahmadfalatin@gmail.com>
2025-03-13 11:23:53 +0700

    perbaikan error js diskon

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -12,7 +12,7 @@
 
         <!-- Program Service Menu Item -->
         @if (Gate::allows('isAdminOrEngineer') xor Gate::allows('isKasir'))
-        <li class="nav-item {{ request()->is('customer.index') ? 'active' : '' && request()->is('service*') ? 'active' : '' }} animate__animated animate__slideInLeft animate__delay-3.3s">
+        <li class="nav-item {{ request()->is('service*') ? 'active' : '' }} animate__animated animate__slideInLeft animate__delay-3.3s">
             <a class="nav-link" href="{{ route('customer.index') }}">
                 <i class="mdi mdi-cog-outline menu-icon"></i>
                 <span class="menu-title">Layanan Servis</span>
